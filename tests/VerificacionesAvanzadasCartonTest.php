d5e1d36d94d5c34a7e17195a57a749d74f73de91
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-06-12 00:01:43 -0300

    test copiados

--- tests/VerificacionesAvanzadasCartonTest.php
+++ tests/VerificacionesAvanzadasCartonTest.php
@@ -10,20 +10,53 @@ class VerificacionesAvanzadasCartonTest extends TestCase {
    * Verifica que los números del carton se encuentren en el rango 1 a 90.
    */
   public function testUnoANoventa() {
-    $this->assertTrue(TRUE);
+    
+    $bool = TRUE;
+    $carton=new CartonEjemplo;
+    foreach ($carton->filas() as $fila) {
+      foreach ($fila as $celda) {
+        if($celda == 0){
+          continue;
+        }
+        if ($celda < 1 || $celda > 90) {
+          $bool = FALSE;
+        }
+      }
+    }
+    $this->assertTrue($bool);
   }
 
   /**
    * Verifica que cada fila de un carton tenga exactamente 5 celdas ocupadas.
    */
   public function testCincoNumerosPorFila() {
-    $this->assertTrue(TRUE);
+    
+    $carton=new CartonEjemplo;
+    foreach($carton->filas() as $fila) {
+      $this->assertCount(5,array_filter($fila));
+    }
   }
 
   /**
    * Verifica que para cada columna, haya al menos una celda ocupada.
    */
   public function testColumnaNoVacia() {
+
+    $carton=new CartonEjemplo;
+    $co = 0;
+    foreach($carton->columnas() as $columna){
+      foreach($columna as $celda){
+        if ($celda != 0){
+        $co=$co+1;
+        }
+      }
+      if($co == 0){
+        $this->assertTrue(FALSE);
+      }
+      else{
+        $co = 0;
+      }
+    }
     $this->assertTrue(TRUE);
   }
 
@@ -31,7 +64,19 @@ class VerificacionesAvanzadasCartonTest extends TestCase {
    * Verifica que no haya columnas de un carton con tres celdas ocupadas.
    */
   public function testColumnaCompleta() {
-    $this->assertTrue(TRUE);
+
+    $carton = new CartonEjemplo;
+    $co=0;
+    foreach($carton->columnas() as $columna) {
+      foreach($columna as $celda){
+        if($celda != 0){
+          $co++;
+        }
+      }
+      $this->assertNotEquals(3, $co);
+      $co = 0; 
+    }
+
   }
 
   /**
@@ -39,7 +84,23 @@ class VerificacionesAvanzadasCartonTest extends TestCase {
    * ocupada.
    */
   public function testTresCeldasIndividuales() {
-    $this->assertTrue(TRUE);
+    
+    $carton = new CartonEjemplo;
+    $co = 0;
+    $co2 = 0;
+    foreach($carton->columnas() as $columna) {
+      $c0 = 0;
+      foreach($columna as $celda){
+        if($celda != 0){
+          $co++;
+        }
+      }
+      if($co==1){
+        $co2++;
+      }
+    }
+    $this->assertTrue($co2==3);
+
   }
 
   /**
@@ -47,14 +108,54 @@ class VerificacionesAvanzadasCartonTest extends TestCase {
    * las columnas a la derecha.
    */
   public function testNumerosIncrementales() {
-    $this->assertTrue(TRUE);
+
+    $max = 0;
+    $cel = 0;
+    foreach($carton->columnas() as $columna){
+      $min = 100;
+      foreach($columna as $celda){
+        if($celda < $min && $celda != 0){
+          $min = $celda;
+        }
+        if ($celda > $cel){
+          $cel = $celda;
+        }
+      }
+      if($min>$max){
+        $this->assertTrue(TRUE);
+      }
+      else{
+        $this->assertTrue(FALSE);
+      }
+      $max = $cel;
+    }
   }
 
   /**
    * Verifica que en una fila no existan más de dos celdas vacias consecutivas.
    */
   public function testFilasConVaciosUniformes() {
-    $this->assertTrue(TRUE);
+
+    $carton = new CartonEjemplo;
+    foreach($carton->filas() as $fila){	
+      $co = 0;
+      foreach($fila as $celda){
+      	if($celda == 0){
+         $co++; 
+        }
+        else{
+         $comax=$co;
+         $co = 0;
+        }
+      }
+      if($comax < 3){
+      $this->assertTrue(TRUE);
+      }
+      else{
+        $this->assertTrue(FALSE);
+      }
+    }
+  }
   }
 
 }
