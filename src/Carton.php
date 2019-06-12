1f1f933498d83d39160c4af53f84296186ebe30c
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-06-12 15:51:30 -0300

    Update Carton.php

--- src/Carton.php
+++ src/Carton.php
@@ -4,7 +4,7 @@ namespace Bingo;
 
 Class Carton implements CartonInterface
 {
-    public $numeros_carton = [];
+    protected $numeros_carton = [];
 
     public function __construct (array $aleatorio)
     {
