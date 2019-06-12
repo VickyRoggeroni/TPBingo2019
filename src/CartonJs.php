88e0f83ba4f131dac9699745dbc52684614b0274
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-06-12 13:11:40 -0300

    Update CartonJs.php

--- src/CartonJs.php
+++ src/CartonJs.php
@@ -22,28 +22,22 @@ class CartonJs implements CartonInterface {
    * {@inheritdoc}
    */
   public function filas() {
-    return [
-      [4,0,24,31,40,0,0,0,80],
-      [0,13,0,39,48,0,66,72,0],
-      [1,0,27,0,0,55,0,73,86],
-    ];
+    return $this->numeros_carton;
   }
 
   /**
    * {@inheritdoc}
    */
   public function columnas() {
-    return [
-      [4,0,1],
-      [0,13,0],
-      [24,0,27],
-      [31,39,0],
-      [40,48,0],
-      [0,0,55],
-      [0,66,0],
-      [0,72,73],
-      [80,0,86],
-    ];
+    $columnas = [];
+    
+    for($i=0; $i<3; $i++)
+    {
+        for($j=0; $j<9; $j++)
+        {
+        $columnas [$j][$i] = $this->numeros_carton;
+        }
+    }
   }
 
   /**
